69fdb9aa02225f92ae8baa2de1f9d1014131b93d
andyvorld <andyvorld@live.com.au>
2020-11-17 14:44:16 +1100

    initial

---
+++ LGSTrayBattery_GHUB_dump/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LGSTrayBattery_GHUB_dump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
